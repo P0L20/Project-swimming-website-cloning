959da3f4850c35802335111acc502687891a73de
Paul Andrei Mendiola <mendiolapaulandrei20@gmail.com>
2025-07-09 08:09:26 +0800

    Front page start

--- js/front-page.js
+++ js/front-page.js
@@ -28,22 +28,28 @@ document.querySelector('.js-review-container')
   
 const personOffers = [{
   image: 'pictures/family.png',
-  text: 'Family swims'
+  text: 'Family swims',
+  icon: '🏡'
 }, {
   image: 'pictures/frieds.png',
-  text: 'Pool parties'
+  text: 'Pool parties',
+  icon: '🥳'
 }, {
   image: 'pictures/solo.png',
-  text: 'Solo self care days'
+  text: 'Solo self care days',
+  icon: '🧖'
 }, {
   image: 'pictures/pool.png',
-  text: '$50 budget fun'
+  text: '$50 budget fun',
+  icon: '🤑'
 }, {
   image: 'pictures/night.png',
-  text: 'Night swims'
+  text: 'Night swims',
+  icon: '✨'
 }, {
   image: 'pictures/pregnant.png',
-  text: 'Photo shoots'
+  text: 'Photo shoots',
+  icon: '📸'
 }];
 
 let personOffersHTML = ''
@@ -52,7 +58,10 @@ personOffers.forEach((personOffer) => {
   personOffersHTML += 
           `<div class="image-container">
             <img class="picture-person-offer" src="${personOffer.image}" alt="picture of family">
-            <p>${personOffer.text}</p>
+            <div>
+              <p>${personOffer.icon}</p>
+              <p>${personOffer.text}</p>
+            </div>
           </div>`
 });
 
@@ -74,5 +83,148 @@ document.querySelector('.js-sign-up')
     signUpCondition = !signUpCondition;
   });
 
-//more than just pools 
+//diff offer
 
+const diffOfferArray = [{
+  pools: [{
+    img: 'diff-offer-pictures/pool-1.png',
+    name: 'Azure Springs Resort',
+    discover: 'pools',
+    place: 'Philippines',
+    star: 671,
+    price: 80
+  },{
+    name: 'Blue Lagoon Retreat',
+    img: 'diff-offer-pictures/pool-2.png',
+    discover: 'pools',
+    place: 'Japan',
+    star: 457,
+    price: 80
+  }]
+}, {
+  tennis: [{
+    img: 'diff-offer-pictures/tennis-1.png',
+    name: 'Tennis Court. Villa',
+    discover: 'court',
+    place: 'Los Angeles',
+    star: 27,
+    price: 27
+  },{
+    img: 'diff-offer-pictures/tennis-2.png',
+    name: 'Tennis Oasis',
+    discover: 'court',
+    place: 'Los Angeles',
+    star: 8,
+    price: 25
+  }]
+}, {
+  pickleball: [{
+    img: 'diff-offer-pictures/pickleball-1.png',
+    name: 'Private Pickleball Paradise In San Diego',
+    discover: 'court',
+    place: 'San Diego',
+    star: 14,
+    price: 35
+  }, {
+    img: 'diff-offer-pictures/pickleball-2.png',
+    name: 'Gorgeous Pickleball Court',
+    discover: 'court',
+    place: 'Los Angeles',
+    star: 18,
+    price: 38
+  }]
+}, {
+  hoops: [{
+    img: 'diff-offer-pictures/bball-1.png',
+    name: 'Beutifully Surfaced Basketball Half-Court',
+    discover: 'court',
+    place: 'Los Angeles',
+    star: 1,
+    price: 80
+  }, {
+    img: 'diff-offer-pictures/bball-2.png',
+    name: 'Ballers Paradise!',
+    discover: 'court',
+    place: 'Vaughan',
+    star: 1,
+    price: 50
+  }]
+}, {
+  indoor: [{
+    img: 'diff-offer-pictures/indoor-1.png',
+    name: 'Ranch-style La Designer Retreat',
+    discover: 'home',
+    place: 'Los Angeles',
+    star: 1,
+    price: 250
+  }, {
+    img: 'diff-offer-pictures/indoor-2.png',
+    name: 'The Temple Hill One',
+    discover: 'home',
+    place: 'Los Angeles',
+    star: 4,
+    price: 1299
+  }]
+}, {
+  backyards: [{
+    img: 'diff-offer-pictures/backyard-1.png',
+    name: 'Luxurious yard',
+    discover: 'backyards',
+    place: 'San Diego',
+    star: 4,
+    price: 12000
+  }, {
+    img: 'diff-offer-pictures/backyard-2.png',
+    name: 'Diola Hill',
+    discover: 'backyards',
+    place: 'Los Angeles',
+    star: 189,
+    price: 30000
+  }]
+}];
+
+document.querySelectorAll('.js-button')
+  .forEach(button => {
+    button.addEventListener('click', (event) => {
+      const option = event.currentTarget.dataset.option;
+        
+
+      let renderHTML = '';
+
+      diffOfferArray.forEach((offer) => {
+        const key = Object.keys(offer)[0];
+        if(key === option) {
+          offer[key].forEach((key) => {
+            document.querySelector('.js-discover-button')
+              .innerHTML = `Discover ${key.discover} near me`
+            renderHTML += renderOffer(key);
+          })
+        }
+      });
+        document.querySelector('.picture-offer')
+          .innerHTML = renderHTML;
+    });
+  });
+
+function renderOffer(option) {
+  return `
+  <div class="pool-container">
+      <img src="${option.img}" alt="pool">
+      <p class="name-pool">${option.name}</p>
+      <p class="place">
+        <i class="fa-solid fa-map-pin"></i>
+        ${option.place}
+      </p>
+      <p class="stars">
+        
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        ${option.star}
+      </p>
+      <p class="price">$${option.price}/hr</p>
+    </div>
+  `
+}
