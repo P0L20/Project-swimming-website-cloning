75556d49c983cd1d6eccce7ef850188948dc6f10
Paul Andrei Mendiola <mendiolapaulandrei20@gmail.com>
2025-07-23 21:12:44 +0800

    make the html clean by removing some html tags then generating it by hosting.js

--- js/arrays-data-hosting.js
+++ js/arrays-data-hosting.js
@@ -39,3 +39,50 @@ export const hostingPic = [{
   lowerText: `Host intimate events sharing your home spaces when you aren't using them.`
   }
 }]
+
+export const infoContainer = [{
+  title: 'Create your listing for free',
+  description: `Set your price, upload a few photos, determine your rules and availability and we'll make your listing live and ready to go.`,
+  link: 'Need help listing?'
+}, {
+  title: 'Accept reservations when you want.',
+  description: `Guests will find your pool and request to book it by the hour. You can chat with guests, read their reviews and ensure you are comfortable with every reservation before accepting.`
+}, {
+  title: 'Get paid quickly.',
+  description: `Once approved, Guests are charged upfront through Swimply's secure payment system. Stripe directly deposits your earnings after each booking, minus our service fee.`
+}];
+
+export const reviewContainer = [{
+  profile: 'pictures/review-host-pic.png',
+  name: 'Lauren from Wantagh, NY',
+  date: '2019',
+  comment: `I love being a pool owner but always thought it a shame the pool went unused for even just one hot summer day. With Swimply, I can share the joy of cooling off in my relaxing pool with my community. And the financial gain certainly doesn't hurt!`,
+  earn: '5,000'
+}, {
+  profile: 'pictures/review-host-pic2.png',
+  name: 'Stuart from Poland, OR',
+  date: '2019',
+  comment: `Most pool owners will tell you how much their pool costs them per month. Swimply pool owners will tell you how much their pool earns them per month!`,
+  earn: '12,000'
+}, {
+  profile: 'pictures/review-host-pic3.png',
+  name: 'Ned & Ann from Aurora, CO',
+  date: '2019',
+  comment: `As far as offsetting some of our pool costs...not only did it totally pay all of our pool expenses, it has also brought in an entirely new revenue stream for our family.`,
+  earn: '7,000'
+}];
+
+export const gotContainer = [{
+  img: 'fa-solid fa-id-card',
+  para1: 'Built on trust',
+  para2: `Swimply Guests add a profile photo, so you know who you're talking to. Hosts and Guests also review each other after every booking for quality assurance.`
+}, {
+  img: 'fa-solid fa-headset',
+  para1: 'Dedicated Host support',
+  para2: `Our dedicated Host Support Team is available 24/7 to assist with anything you may need and coach you on popular trends.`,
+  class: 'card-back-middle'
+}, {
+  img: 'fa-solid fa-shield-halved',
+  para1: 'Up to $1,000,000 of protection',
+  para2: `We've got your peace of mind covered  with up to $1,000,000 protection guarantee and $10,000 of property damage protection.`
+}];
--- js/hosting.js
+++ js/hosting.js
@@ -1,4 +1,4 @@
-import { hostingPic } from "./arrays-data-hosting.js";
+import { hostingPic, infoContainer, reviewContainer, gotContainer } from "./arrays-data-hosting.js";
 import { signUp } from "./sign-up.js";
 import { getLinkInfo } from "./front-page.js";
 
@@ -105,4 +105,75 @@ function goToHosting () {
       button.classList.add(data[dataName].color);
     }
   })
+}
+
+generateInfoHTML();
+generateReviewHTML();
+generateGotHTML();
+
+function generateInfoHTML () {
+  let html = ''
+
+  infoContainer.forEach(info => {
+
+    if(info.link) {
+    html += `<div class="hosting">
+            <p>${info.title}</p>
+            <p>${info.description}</p>
+            <a href="">${info.link}</a>
+          </div>`} else {
+            html += `<div class="hosting">
+            <p>${info.title}</p>
+            <p>${info.description}</p>
+          </div>`
+          }
+  });
+
+  document.querySelector('.how-to-host-container')
+    .innerHTML = html;
+}
+
+function generateReviewHTML () {
+  let html = '';
+
+  reviewContainer.forEach(review => {
+    html += `<div class="card">
+            <img src="${review.profile}">
+            <p class="name-host">${review.name}</p>
+            <p class="since">Host since 2019</p>
+            <p class="comment-host">${review.comment}</p>
+            <div class="earning">
+              <i class="fa-solid fa-piggy-bank"></i>
+              <p>Earning $${review.earn} a month</p>
+            </div>
+          </div>`
+  });
+
+  document.querySelector('.container-card')
+    .innerHTML = html;
+}
+
+function generateGotHTML () {
+  let html = '';
+
+  gotContainer.forEach(got => {
+
+    if(got.class) {
+      html += `<div class="card-back ${got.class}">
+            <i class="${got.img}"></i>
+            <p>${got.para1}</p>
+            <p>${got.para2}</p>
+          </div>`
+    } else {
+      html += `<div class="card-back">
+              <i class="${got.img}"></i>
+              <p>${got.para1}</p>
+              <p>${got.para2}</p>
+            </div>`
+    }
+  });
+
+  document.querySelector('.container-card-back')
+    .innerHTML = html;
+
 }
